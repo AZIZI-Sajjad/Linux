fa72bfcfe8f8ae1b8a16e79a5354b31614a446d6
AZIZI-Sajjad <77125737+AZIZI-Sajjad@users.noreply.github.com>
2023-11-17 00:44:46 +0100

    Update get-publique-ip-send-by-mail.sh

--- get-publique-ip-send-by-mail/get-publique-ip-send-by-mail.sh
+++ get-publique-ip-send-by-mail/get-publique-ip-send-by-mail.sh
@@ -1,3 +1,16 @@
+# sudo cat /home/scripts/get-ip-send-by-mail.sh
+# 
+senderMail="senderMail@exemple.com"
+
+
+# To set destinations addresses mails at prompt decomment "recipients=$1" & comment "repipients"
+# Exemple:
+# /home/scripts/get-ip-send-by-mail.sh  "recipient1@example.com recipient2@example.com recipient3@example.com recipient4@example.com"
+# recipients=$1
+recipients="recipient1@example.com recipient2@example.com"
+
+mailsubject="YOUR IP home at $(date +"%Y-%m-%d-%Hh%M:%Ss")"
+
 # Effectuer la requête curl et stocker la réponse dans une variable
 response=$(curl -k monip.org)
 
@@ -6,13 +19,13 @@ ip_line=$(echo "$response" | grep "IP :")
 
 # Utiliser sed pour extraire l'adresse IP de la ligne
 ip=$(echo "$ip_line" | sed -n 's/.*IP : \([0-9.]*\).*/\1/p')
+mailbody=$ip
 
 # Afficher l'adresse IP
 echo "Adresse IP : $ip"
 
 
-echo -e "Subject: YOUR IP at $(date +"%Y-%m-%d-%Hh%M:%Ss")\nFrom: NOREPLY Your Name <your@email.com>\n\nYOUR IP at  $(date +"%Y-%m-%d-%Hh%M:%Ss") is :\n $ip" | msmtp recipient1@example.com
-echo -e "Subject: YOUR IP at $(date +"%Y-%m-%d-%Hh%M:%Ss")\nFrom: Your Name <your@email.com>\n\nYOUR IP at  $(date +"%Y-%m-%d-%Hh%M:%Ss") is :\n $ip" | msmtp recipient2@example.com
-
-
+for recipient in $recipients; do
+        echo -e $mailbody | mail -s "$mailsubject" -aFrom:$senderMail $recipient
+done
 
