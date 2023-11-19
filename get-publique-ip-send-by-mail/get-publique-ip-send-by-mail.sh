b7ea50ec590b25d809002693e7afe8a9d6eb2cc6
AZIZI-Sajjad <77125737+AZIZI-Sajjad@users.noreply.github.com>
2023-11-20 00:40:49 +0100

    Update get-publique-ip-send-by-mail.sh

--- get-publique-ip-send-by-mail/get-publique-ip-send-by-mail.sh
+++ get-publique-ip-send-by-mail/get-publique-ip-send-by-mail.sh
@@ -2,6 +2,12 @@
 # 
 senderMail="senderMail@exemple.com"
 
+# Set IP public manually, in this case comment lines "6 & 7" 
+# lastip="87.164.55.189"
+lastip_file=./lastip_file
+lastip=$(cat "$lastip_file")
+echo "lastip : $lastip"
+echo "#######################"
 
 # To set destinations addresses mails at prompt decomment "recipients=$1" & comment "repipients"
 # Exemple:
@@ -17,15 +23,36 @@ response=$(curl -k monip.org)
 # Utiliser grep pour rechercher la ligne contenant "IP :" et extraire l'adresse IP
 ip_line=$(echo "$response" | grep "IP :")
 
-# Utiliser sed pour extraire l'adresse IP de la ligne
-ip=$(echo "$ip_line" | sed -n 's/.*IP : \([0-9.]*\).*/\1/p')
-mailbody=$ip
-
-# Afficher l'adresse IP
-echo "Adresse IP : $ip"
-
-
-for recipient in $recipients; do
+# Define Function to sending mail: 
+function sendmail(){
+    # Get Mail Body from prompt.
+    # In this case, In the function verify_ip, when we call this function, w'll capable to set and get MailBody's value in another function
+    # For more details see the 45th & 50th lines 
+    mailbody="$1"
+    for recipient in $recipients; do
         echo -e $mailbody | mail -s "$mailsubject" -aFrom:$senderMail $recipient
-done
+        echo "Mail sent to $recipient"
+    done
+}
 
+
+function verify_ip(){
+    if [ $ip == $lastip ]
+    then
+        mailbody="IP Not Chnaged IP : $ip "
+	echo $mailbody
+        # Call sendmail Function and set MailBody, Also see what is explained on the lines 28 -> 30
+	sendmail "$mailbody"
+    else
+   	mailbody="IP Chnaged / New IP : $ip"
+	echo $mailbody
+        # Call sendmail Function and set MailBody, Also see what is explained on the lines 28 -> 30
+	sendmail "$mailbody"
+    echo "$ip" > "$lastip_file"
+    echo "Updating lastip to: $ip"
+    fi
+}
+
+
+
+verify_ip
