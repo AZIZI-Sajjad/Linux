0500724972490ec80e42908d2cddef537c873c0f
AZIZI-Sajjad <77125737+AZIZI-Sajjad@users.noreply.github.com>
2024-12-28 19:31:53 +0100

    Update backup-and-compress-and-send-by-mail.sh 20241228

--- backup-and-compress-and-send-by-mail/backup-and-compress-and-send-by-mail.sh
+++ backup-and-compress-and-send-by-mail/backup-and-compress-and-send-by-mail.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Récupérer le PATH du script
+script_dir=$(dirname "$(realpath "$0")")
+
 # .env : Le fichiers des variables dans le même répertoire 
-source .env
+source $script_dir/.env
+# source /var/scripts/bkp-db-vault/.env
 
 echo "#######################"
+echo "script_dir : $script_dir"
 echo "Afficher les variables"
 echo "secret : $secret"
+echo "USER : $USER"
 echo "recipients : $recipients"
 echo "mailsubject : $mailsubject"
 echo "source_Dir : $source_Dir"
@@ -15,14 +21,26 @@ echo "output_File : $output_File"
 echo "RETENTION : $RETENTION jours"
 echo "#######################"
 
-
-log_debug_information() {
-    echo "$(date +"%Y-%m-%d-%Hh%M-%Ss") - [Debug-Information] : $1" | tee -a $log_File
+function log_debug_information() {
+    echo "$(date +"%Y-%m-%d-%Hh%M-%Ss") - [Debug-Information] [Current-User: $USER] : $1" | tee -a $log_File
 }
 
-log_debug_error() {
-    echo "$(date +"%Y-%m-%d-%Hh%M-%Ss") - [Debug-Error] : $1" | tee -a $log_File
+
+function log_debug_error() {
+    echo "$(date +"%Y-%m-%d-%Hh%M-%Ss") - [Debug-Error] [Current-User: $USER] : $1" | tee -a $log_File
 }
+
+
+function check_user_or_cron(){
+    # Vérifier si le script est exécuté par cron
+    # L'option -n dans une condition if teste si la chaîne spécifiée est non vide. Autrement dit, cela vérifie si la variable $CRON_TZ contient une valeur.
+    if [ -z "$PS1" ] && [ -z "$SSH_CONNECTION" ] && [ -z "$TMUX" ] && [ -z "$USER" ]; then
+    log_debug_information "Le script a été lancé par Cron."
+    else
+    log_debug_information "Le script a été lancé par un utilisateur : $USER"
+    fi
+}
+
 
 function create_log_dir(){
     # Vérification du nombre d'arguments
@@ -35,9 +53,8 @@ function create_log_dir(){
         log_debug_information "Dossier de log '$log_Dir' existe" | tee $log_File
 
     fi
-
-
 }
+
 
 function sendmail(){
     # Vérification du nombre d'arguments
@@ -53,11 +70,11 @@ function sendmail(){
     attachement="$5"
     log_File="$6"
     log_debug_information ": Lancement de la fonction Afficher les variables dans la fonction 'sendmail'." | tee $log_File
-    log_debug_information ": recipients dnas la fonction archive : $recipients" | tee $log_File
-    log_debug_information ": mailbody dnas la fonction archive : $mailbody" | tee $log_File
-    log_debug_information ": mailsubject dnas la fonction archive : $mailsubject" | tee $log_File
-    log_debug_information ": senderMail dnas la fonction archive : $senderMail" | tee $log_File
-    log_debug_information ": attachement dnas la fonction archive : $attachement" | tee $log_File
+    log_debug_information ": recipients dans la fonction archive : $recipients" | tee $log_File
+    log_debug_information ": mailbody dans la fonction archive : $mailbody" | tee $log_File
+    log_debug_information ": mailsubject dans la fonction archive : $mailsubject" | tee $log_File
+    log_debug_information ": senderMail dans la fonction archive : $senderMail" | tee $log_File
+    log_debug_information ": attachement dans la fonction archive : $attachement" | tee $log_File
 
     if [  -d "$source_Dir" ]; then
         log_debug_information "Le dossier cible $source_Dir existe." | tee $log_File
@@ -93,9 +110,9 @@ function archive(){
     output_File="$3"
     log_File="$4"
     log_debug_information "Lancement de la fonction Afficher les variables dans la fonction 'archive'." | tee $log_File
-    log_debug_information "secret dnas la fonction archive : $secret" | tee $log_File
-    log_debug_information "source_Dir dnas la fonction archive : $source_Dir" | tee $log_File
-    log_debug_information "output_File dnas la fonction archive : $output_File" | tee $log_File
+    # log_debug_information "secret dans la fonction archive : $secret" | tee $log_File
+    log_debug_information "source_Dir dans la fonction archive : $source_Dir" | tee $log_File
+    log_debug_information "output_File dans la fonction archive : $output_File" | tee $log_File
 
     if [  -d "$source_Dir" ]; then
         log_debug_information "Le dossier cible $source_Dir existe." | tee $log_File
@@ -109,13 +126,11 @@ function archive(){
     fi
 
     log_debug_information "Début de compression" | tee $log_File
-    # sudo apt update && sudo apt install -y p7zip-full
-
 
     log_debug_information "Compression et chiffrement du dossier..." | tee $log_File
+    # sudo apt update && sudo apt install -y p7zip-full
     # -9 : Cmpression maximale 
     zip -r -e -P "$secret" -9 "$output_File" "$source_Dir"
-
 
     log_debug_information "Début de chiffrement" | tee $log_File
 }
@@ -130,6 +145,8 @@ function retention() {
     find $backup_Dir/* -mtime +$RETENTION -delete
 }
 
+# Exécuter le script et faire appel aux fonctions
+check_user_or_cron
 create_log_dir
 archive  "$secret" "$source_Dir" "$output_File" "$log_File"
 sendmail  "$recipients" "$mailbody" "$mailsubject" "$senderMail" "$output_File" "$log_File"
